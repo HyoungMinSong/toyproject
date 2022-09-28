be8eecce850e540a2871ea64d1459c82608ef78e
HyoungMinSong <shmin11@naver.com>
2022-09-29 01:41:37 +0900

    RDS 환경 profile 설정 추가

--- src/test/java/gameshop/toy/controller/IndexControllerTest.java
+++ src/test/java/gameshop/toy/controller/IndexControllerTest.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import javax.transaction.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class IndexControllerTest {
 
     @Autowired
